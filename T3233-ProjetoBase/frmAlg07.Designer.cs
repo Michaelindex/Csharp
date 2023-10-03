ab17b28e6d559e1d41ce3d575c8f6dea34cd0c05
Michael Santos <mcialbr123@gmail.com>
2023-10-03 11:25:46 -0300

    att

---
+++ T3233-ProjetoBase/frmAlg07.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace T3233_ProjetoBase
+{
+    partial class frmAlg07
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Somatória de 1 até : ";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(216, 14);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(304, 31);
+            button1.TabIndex = 2;
+            button1.Text = "Somar o intervalo ( FOR )";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 109);
+            button2.Name = "button2";
+            button2.Size = new Size(304, 31);
+            button2.TabIndex = 3;
+            button2.Text = "Somar o intervalo ( GAUSS )";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(336, 14);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(292, 409);
+            listBox1.TabIndex = 4;
+            // 
+            // frmAlg07
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(673, 448);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "frmAlg07";
+            Text = "Algoritmo07";
+            Load += frmAlg07_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+    }
+}
