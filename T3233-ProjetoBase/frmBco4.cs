4e7a75f4104e783a507e6d6ea05082b2921c3518
Michael Santos <mcialbr123@gmail.com>
2023-10-19 11:27:50 -0300

    att

--- T3233-ProjetoBase/frmBco4.cs
+++ T3233-ProjetoBase/frmBco4.cs
@@ -28,6 +28,52 @@ ConfigurationManager.ConnectionStrings[2].ConnectionString;
         #region Métodos Criados
 
         #endregion
+        private void resetaTela()
+        {
+            btnAdicionar.Enabled = true;
+            btnSalvar.Enabled = false;
+            btnCancelar.Enabled = false;
+            btnExcluir.Enabled = false;
+
+            lblCodigo.Text = string.Empty;
+
+            foreach(Control control in this.Controls)
+            {
+                if (control is TextBox)
+                {
+                    TextBox textBox = (TextBox)control;
+                    textBox.Text = string.Empty;
+                    textBox.Enabled = false;
+                }
+            }
+        }
+
+        private void carregaGrid()
+        {
+            string sql = "SELECT codcli, nome ,cpf, rg, rua, telefone, cidade, cep, bairro,estado " +
+                "FROM Clientes WHERE (nome + cpf + rg + rua + telefone + cidade + cep + bairro + estado " +
+                "LIKE '%' + @PESQUISA + '%'";
+            SqlConnection con = new SqlConnection(conexao);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@PESQUISA", txtPesquisa.Text);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            try
+            {
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                dgvDados.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+                
+            }
+        }
 
         #region Botões
 
