084b090902978fa0ea440b031fe6ceff3fef430c
Michael Santos <mcialbr123@gmail.com>
2023-10-24 10:43:01 -0300

    att

---
+++ T3233-ProjetoBase/frmExtra1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace T3233_ProjetoBase
+{
+    partial class frmExtra1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnResposta = new Button();
+            label1 = new Label();
+            txtPalpite = new TextBox();
+            SuspendLayout();
+            // 
+            // btnResposta
+            // 
+            btnResposta.Location = new Point(43, 84);
+            btnResposta.Name = "btnResposta";
+            btnResposta.Size = new Size(210, 52);
+            btnResposta.TabIndex = 5;
+            btnResposta.Text = "Confirmar Palpite";
+            btnResposta.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(34, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(336, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Tente adivinhar o número secreto ( de 1 a 100 )";
+            // 
+            // txtPalpite
+            // 
+            txtPalpite.Location = new Point(43, 41);
+            txtPalpite.Name = "txtPalpite";
+            txtPalpite.Size = new Size(100, 23);
+            txtPalpite.TabIndex = 3;
+            // 
+            // frmExtra1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(403, 195);
+            Controls.Add(btnResposta);
+            Controls.Add(label1);
+            Controls.Add(txtPalpite);
+            Name = "frmExtra1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Extra 1 - Jogo da Adivinhação";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnResposta;
+        private Label label1;
+        private TextBox txtPalpite;
+    }
+}
---
+++ T3233-ProjetoBase/frmExtra1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T3233_ProjetoBase
+{
+    public partial class frmExtra1 : Form
+    {
+        public frmExtra1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class JogoAdivinhacao
+    {
+        private int numeroSecreto;
+        private Random random;
+
+        public JogoAdivinhacao()
+        {
+            random = new Random();
+            GerarNumeroSecreto();
+        }
+
+        public void GerarNumeroSecreto()
+        {
+            numeroSecreto = random.Next(1, 101);
+        }
+
+        public string VerificarPalpite(int palpite)
+        {
+            if (palpite < numeroSecreto)
+            {
+                return "Muito baixo! Tente novamente.";
+            }
+            else if (palpite > numeroSecreto)
+            {
+                return "Muito alto! Tente novamente.";
+            }
+            else
+            {
+                return "Correto!";
+            }
+        }
+
+    }
+}
